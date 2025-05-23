02f2baf51a95500fece6448fc5e8aecbe7653afb
ANURAG YADAV <139148328+mrcutex1@users.noreply.github.com>
2025-05-23 23:06:57 +0530

    Update config.py

--- config.py
+++ config.py
@@ -21,8 +21,9 @@ DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 120))
 # Vars For API End Pont.
 #YTPROXY_URL = getenv("YTPROXY_URL", 'http://tubed.okflix.top') ## E.G https://yt.okflix.
 #YT_API_KEY = "freekey15052025"
-API_URL = getenv("API_URL", 'https://api.thequickearn.xyz') #youtube song url
-API_KEY = getenv("API_KEY", '30DxNexGenBotsc0db7b') # youtube song api key, get it from https://t.me/RahulTC
+# Vars For API End Pont.
+YTPROXY_URL = getenv("YTPROXY_URL", 'https://tgapi.xbitcode.com') ## E.G https://yt.okflix.
+YT_API_KEY = getenv("YT_API_KEY",None)##get it from xbitcode
 
 # Chat id of a group for logging bot's activities
 LOGGER_ID = int(getenv("LOGGER_ID", "-1002133369721"))
