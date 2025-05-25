462265d6e168855c28a4b6a742a0c19b677d4362
ANURAG YADAV <139148328+mrcutex1@users.noreply.github.com>
2025-05-25 15:35:11 +0530

    Update logging.py

--- AnonXMusic/logging.py
+++ AnonXMusic/logging.py
@@ -5,16 +5,18 @@ logging.basicConfig(
     format="[%(asctime)s - %(levelname)s] - %(name)s - %(message)s",
     datefmt="%d-%b-%y %H:%M:%S",
     handlers=[
-        logging.FileHandler("log.txt"),
+        logging.FileHandler("logs.txt"),  # Changed from log.txt to logs.txt
         logging.StreamHandler(),
     ],
 )
 
-logging.getLogger("httpx").setLevel(logging.ERROR)
-logging.getLogger("pymongo").setLevel(logging.ERROR)
-logging.getLogger("pyrogram").setLevel(logging.ERROR)
-logging.getLogger("pytgcalls").setLevel(logging.ERROR)
-
+logging.getLogger("httpx").setLevel(logging.CRITICAL) 
+logging.getLogger("pyrogram").setLevel(logging.CRITICAL)
+logging.getLogger("pytgcalls").setLevel(logging.CRITICAL)
+logging.getLogger("asyncio").setLevel(logging.WARNING)
+logging.getLogger("async").setLevel(logging.ERROR)
+logging.getLogger("pymongo").setLevel(logging.CRITICAL)
+logging.getLogger("AnonXMusic.platforms.Youtube").setLevel(logging.ERROR)
 
 def LOGGER(name: str) -> logging.Logger:
     return logging.getLogger(name)
